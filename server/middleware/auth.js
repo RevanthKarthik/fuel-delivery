14cd4caf096556d3127c41ecf429935d1861e488
RevanthKarthik <revanthkarthik026@gmail.com>
2025-07-23 15:06:04 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,6 @@
 // server/middleware/auth.js
 import jwt from 'jsonwebtoken';
-import User from '../models/user.js';
+import User from '../models/User.js';
 
 
 export const requireAuth = async (req, res, next) => {
