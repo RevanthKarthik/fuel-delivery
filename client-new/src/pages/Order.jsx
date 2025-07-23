ebc2d074f98d881ea1ed38868d52c82835616ba1
RevanthKarthik <revanthkarthik026@gmail.com>
2025-07-23 16:24:40 +0530

    Update Order.jsx

--- client-new/src/pages/Order.jsx
+++ client-new/src/pages/Order.jsx
@@ -20,7 +20,7 @@ const navigate = useNavigate();
     e.preventDefault();
 
     try {
-      await axios.post('https://fuel-delivery-backend-98bj.onrender.com0/api/orders', form, {
+      await axios.post('https://fuel-delivery-backend-98bj.onrender.com/api/orders', form, {
         headers: {
           Authorization: `Bearer ${token}`,
         }
