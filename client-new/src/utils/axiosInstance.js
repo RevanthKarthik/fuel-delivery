b7654d326d2234d8595fb6f4ff70dcc1e55389a0
Revanth Karthik <revanthkarthik026@gmail.com>
2025-07-26 16:06:39 +0530

    Update axiosInstance.js

--- client-new/src/utils/axiosInstance.js
+++ client-new/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://fuel-delivery-backend-98bj.onrender.com',
 });
 
 // Automatically log user out on 401 or network errors
