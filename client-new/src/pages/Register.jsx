a64fd24e7131349364d0d2c3e5f9fbb5f59f49fb
Revanth Karthik <revanthkarthik026@gmail.com>
2025-07-31 16:41:17 +0530

    Update Register.jsx

--- client-new/src/pages/Register.jsx
+++ client-new/src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import  { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { API_BASE_URL } from '../api';
 import 'bootstrap/dist/css/bootstrap.min.css';
